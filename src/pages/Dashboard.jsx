6d983390cbae3a68e48bcf7d726c2cbc07a53892
sujoy-voyantcs <sujoy.s@voyantcs.com>
2026-04-09 14:50:11 +0000

    datalist search filter and datalist loader implemented

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Card, Container, Row, Col, Form } from "react-bootstrap";
-
+import "../index.css";
 function Dashboard() {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
 
   // API call
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(res => res.json())
-      .then(data => setPosts(data));
+      .then(data => {
+        setPosts(data);
+        setLoading(false);
+      });
       
   }, []);
 
@@ -17,6 +22,20 @@ function Dashboard() {
   const filteredPosts = posts.filter(post =>
     post.title.toLowerCase().includes(search.toLowerCase())
   )
+
+  //loading
+  const SkeletonCard = () => {
+  return (
+      <Card className="h-100 shadow-sm">
+        <Card.Body>
+          <div className="skeleton skeleton-title"></div>
+          <div className="skeleton skeleton-text"></div>
+          <div className="skeleton skeleton-text"></div>
+          <div className="skeleton skeleton-text"></div>
+        </Card.Body>
+      </Card>
+    );
+  };
 
   return (
     <Container>
@@ -33,16 +52,31 @@ function Dashboard() {
       </Form>
 
       <Row>
-        {filteredPosts.map((post) => (
-          <Col md={6} className="mb-3" key={post.id}>
-            <Card className="h-100 shadow-sm">
-              <Card.Body>
-                <Card.Title>{post.title}</Card.Title>
-                <Card.Text>{post.body}</Card.Text>
-              </Card.Body>
-            </Card>
+      {loading ? (
+        [...Array(6)].map((_, i) => (
+          <Col md={6} className="mb-3" key={i}>
+            <SkeletonCard />
           </Col>
-        ))}
+        ))
+        ) : filteredPosts.length > 0 ? (
+          filteredPosts.map((post) => (
+            <Col md={6} className="mb-3" key={post.id}>
+              <Card className="h-100 shadow-sm">
+                <Card.Body>
+                  <Card.Title>
+                    <Link to={'/post/${post.id}'}>{post.title}</Link>
+                  </Card.Title>
+                  <Card.Text>{post.body}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))
+        ):(
+          <Col md={12}>
+            <h4 className="text-center mt-4">No Data Found 😢</h4>
+          </Col>
+        )
+      }
       </Row>
 
       <hr />
